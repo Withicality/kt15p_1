c899cc984bc8814445ba20f63ea4232534c23310
ItsWitherMC <54366349+ItsWitherMC@users.noreply.github.com>
2022-10-09 09:57:06 +0700

    clm thieu

--- bai8.cpp
+++ bai8.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main() {
     long long a,b;
+    cout << "Nhap lan luot so kg gao/dan va dan so mot nuoc: ";
     cin >> a >> b;
 
     long long r = a*b;
-    cout << r;
+    cout << "So gao toi thieu can du tru: " << r;
+
 
     return 0;
 }
